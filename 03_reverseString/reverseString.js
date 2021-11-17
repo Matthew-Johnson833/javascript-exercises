71d7cc71ce256ae4b371521121b503dc9fdbf617
Matthew-Johnson833 <matthew.johnson833@gmail.com>
2021-11-16 18:46:11 -0800

    completed and passed all test cases through reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
+    const strArray = str.split("");
+    const reversedArray = strArray.reverse();
+    const string = reversedArray.join("");
 
+    let newString = string.toString();
+    return newString;
 };
 
 // Do not edit below this line
